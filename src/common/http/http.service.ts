2f34cdf132483b9c762353547111ad65ca353c8f
chiefnaheem <naheemademola21@gmail.com>
2023-10-28 00:08:45 -0700

    http service

--- src/common/http/http.service.ts
+++ src/common/http/http.service.ts
@@ -1,20 +1,28 @@
 import { HttpService as NestHttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
+import serverConfig from 'src/database/config/env.config';
 
 @Injectable()
 export class HttpService {
   private readonly logger = new Logger(HttpService.name);
   constructor(private readonly httpService: NestHttpService) {}
 
-  async request<T>(config: any): Promise<T> {
+  async request<T>(method: string, data?: any): Promise<T> {
     try {
-      const response = await this.httpService.request<T>(config).toPromise();
+      const response = await this.httpService
+        .request({
+          url: serverConfig.PAYSTACK_URL,
+          method,
+          data,
+          headers: {
+            Authorization: `Bearer ${serverConfig.PAYSTACK_SECRET_KEY}`,
+          },
+        })
+        .toPromise();
       return response.data;
     } catch (error) {
       this.logger.error(error);
       throw error;
     }
   }
-
-
 }

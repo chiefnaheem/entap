72a092df8e8535c919ed4033ff4fbf82d9b5082b
chiefnaheem <naheemademola21@gmail.com>
2023-10-28 04:30:18 -0700

    transaction entity

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,37 +1,42 @@
-import { BaseEntity } from "src/common/entity/base.entity";
-import { User } from "src/user/entities/user.entity";
-import { Column, Entity, ManyToOne } from "typeorm";
-import { TransactionStatus } from "../enum/transaction.enum";
+import { BaseEntity } from 'src/common/entity/base.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { TransactionStatus } from '../enum/transaction.enum';
 
 @Entity({
-    name: 'transaction',
+  name: 'transaction',
+})
+export class Transaction extends BaseEntity {
+  @ManyToOne(() => User, (user) => user.wallets, {
+    onDelete: 'CASCADE',
+    nullable: false,
   })
-  export class Transaction extends BaseEntity {
-      @ManyToOne(() => User, (user) => user.wallets, { onDelete: 'CASCADE', nullable: false })
-      senderId: string;
+  sender: string;
 
-        @ManyToOne(() => User, (user) => user.wallets, { onDelete: 'CASCADE', nullable: false })
-        receiverId: string;
+  @ManyToOne(() => User, (user) => user.wallets, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  receiver: string;
 
-        @Column({ nullable: false })
-        amount: number;
+  @Column({ nullable: false })
+  amount: number;
 
-        @Column({ nullable: false, enum: Object.values(TransactionStatus) })
-        status: string;
+  @Column({ nullable: false, enum: Object.values(TransactionStatus) })
+  status: string;
 
-        @Column({ nullable: false })
-        currency: string;
+  @Column({ nullable: false })
+  currency: string;
 
-        @Column({ nullable: false })
-        reference: string;
+  @Column({ nullable: false })
+  reference: string;
 
-        @Column({ nullable: false })
-        narration: string;
+  @Column({ nullable: false })
+  narration: string;
 
-        @Column({ nullable: false })
-        senderBalance: number;
+  @Column({ nullable: false })
+  senderBalance: number;
 
-        @Column({ nullable: false })
-        receiverBalance: number;
-    }
-
+  @Column({ nullable: false })
+  receiverBalance: number;
+}
